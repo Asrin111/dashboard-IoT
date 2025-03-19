d632d76e48631cc32b4b67216c94edb9cda32bca
Asrini Muhsin <asrinimuhsin@gmail.com>
2025-03-20 02:34:31 +0800

    Menambahkan semua file proyek

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard',[DashboardController::class,'IndexPage']);
