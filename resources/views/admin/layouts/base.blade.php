d1a199297b39b89d48dcbe0e69317dd6069a3ded
Asrini Muhsin <asrinimuhsin@gmail.com>
2025-04-10 01:59:28 +0800

    memperbaiki tampilan

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -31,7 +31,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
@@ -42,7 +42,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{ route('dashboard') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -64,8 +64,8 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="buttons.html">SmartHome</a>
-                        <a class="collapse-item" href="cards.html">SmartCity</a>
+                        <a class="collapse-item" href="{{ route('project.detail', ['id' => 1]) }}">SmartHome</a>
+                        <a class="collapse-item" href="{{ route('project.detail.city', ['id' => 2]) }}">SmartCity</a>
                     </div>
                 </div>
             </li>
